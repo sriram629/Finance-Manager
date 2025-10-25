84b4845928bf63a819bb096e203d6c387bf6e6b9
SRI RAMA KRISHNA CHOWDARY MADDIPATI <sriram@Mac.attlocal.net>
2025-10-25 16:46:41 -0500

    swagger update

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -12,7 +12,9 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
+        url:
+          `https://finance-manager-server-m3o4.onrender.com/` ||
+          `http://localhost:${process.env.PORT || 5000}`,
         description: "Development server",
       },
     ],
